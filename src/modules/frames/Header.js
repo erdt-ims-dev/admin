1032a6652186925c4dcce7dd62cc3600512e92f5
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2023-01-27 16:47:46 +0800

    modified

--- src/modules/frames/Header.js
+++ src/modules/frames/Header.js
@@ -1,36 +1,7 @@
 import React, { Component } from 'react'
 import { Navbar, Container, Breadcrumb } from 'react-bootstrap'
-import image from '../../assets/img/Logowhite.png'
 import './style.css'
-const link = [
-  {
-    title: 'Guides',
-    route: '',
-  },
-  {
-    title: 'Charges',
-    route: '',
-  },
-  {
-    title: 'Locations',
-    route: '',
-  },
-  {
-    title: 'Services',
-    route: '',
-  },
-  {
-    title: 'Be our Partner',
-    route: '',
-  },
-  {
-    title: 'Promos',
-    route: '',
-  },
-  {
-    title: 'Stories',
-    route: '',
-  },]
+
 export class Header extends Component {
     constructor(props) {
         super(props);
@@ -40,29 +11,8 @@ export class Header extends Component {
       }
     render() {
         return (
-            <div className='headerContainer'>
-              <div className='headerLogo'>
-                <img src={image} style={{
-                width: 100,
-                height: 100
-                }}/>
-                
-              </div>
-              <div className='blank'></div>
-              <div className='headerLinks'>
-                {
-                  link.map((item, index) => {
-                   return(
-                    <div>
-                      <p style={{
-                        cursor: 'pointer',
-                        margin: '10px'
-                      }}>{item.title}</p>
-                    </div>
-                   ) 
-                  }
-                )}
-              </div>
+            <div>
+              
             </div>
         )
     }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,10 @@ import React from 'react';
 import {Switch} from 'react-router-dom';
 import Route from './route'
 import Landing from '../modules/landing/index'
-import Test from '../modules/landing/test'
 export default function Routes(){
 	return(
 		<Switch>
 			<Route path="/" exact component={Landing}/>
-			<Route path="/test" exact component={Test}/>
 		</Switch>
 	)
 }
