8807f0a100786ecaacc97a4d0fdae950b2702afc
kmlcburce <45752669+kmlcburce@users.noreply.github.com>
2023-02-01 20:22:38 +0800

    update

---
+++ src/modules/auth/signup.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { ListGroup } from 'react-bootstrap'
+import './style.css'
+
+class Signup extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          data: null
+        };
+      }
+    render() {
+        return (
+            <div>
+              <h1>Signup Page</h1>
+            </div>
+
+        )
+    }
+}
+
+export default Signup
