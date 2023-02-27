d41f8994b4dc37212fe4afdf5da4e25ac1fa226e
comar1 <officialfranciscomar@gmail.com>
2023-02-27 20:47:53 +0800

    application module

---
+++ src/modules/applications/endorsedapplicant.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import './applications.css'
+
+
+class Endorsedapplicant extends Component {
+
+    render() {
+        return (
+            <div>
+                <p>This is endorsed applicant page</p>
+            </div>
+        )
+    }
+}
+
+export default Endorsedapplicant
