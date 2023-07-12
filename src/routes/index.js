b87bc4e64931a1a59c898cc69a461530154e04f1
comar1 <officialfranciscomar@gmail.com>
2023-07-12 15:14:12 +0800

    changed: applicantlist, added: viewapplicantion

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,10 +3,11 @@ import {Switch} from 'react-router-dom';
 import Route from './route'
 import Dashboard from '../modules/dashboard/index'
 import Login from '../modules/auth/login'
-import EndorsedApplicant from '../modules/applications/EndoresedApplicant'
-import ApplicantList from '../modules/applications/ApplicantList'
+import EndorsedApplicant from '../modules/applications/EndorsedApplicant'
+import AdminApplicantList from '../modules/applications/AdminApplicantList'
 import AccountList from '../modules/applications/AccountList'
-import AdminLeaveRequest from '../modules/applications/AdminLeaveRequest'
+import AdminLeaveRequest from '../modules/applications/AdminLeaveRequests'
+import AdminViewScholar from '../modules/applications/AdminViewScholar.js'
 
 export default function Routes(){
 	return(
@@ -14,9 +15,10 @@ export default function Routes(){
 			<Route path="/" exact component={Login}/>
 			<Route path="/dashboard" exact component={Dashboard}/>
 			<Route path="/endorsed_applicant" exact component={EndorsedApplicant}/>
-			<Route path="/applicant_list" exact component={ApplicantList}/>
+			<Route path="/admin_applicantlist" exact component={AdminApplicantList}/>
 			<Route path="/account_list" exact component={AccountList}/>
 			<Route path="/admin_leaverequest" exact component={AdminLeaveRequest}/>
+			<Route path="/admin_viewscholar" exact component={AdminViewScholar}/>
 		</Switch>
 	)
 }
