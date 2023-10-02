8ec996fcc322282ca88a36d6bb553e5c2799f845
kmlcburce <kmlcburce@gmail.com>
2023-10-02 16:13:33 +0800

    /settings css animations

---
+++ src/modules/generic/input.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react'
+import { ListGroup } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+
+import './style.css'
+
+class InputField extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          data: null,
+          focussed: (props.locked && props.focussed) || false,
+            value: props.value || '',
+            error: props.error || '',
+            label: props.label || '',
+        };
+      }
+      onChange = event => {
+        const { id } = this.props;
+        const value = event.target.value;
+        this.setState({ value, error: '' });
+        return this.props.onChange(id, value);
+      }
+    render() {
+        const {id, type, placeholder, locked} = this.props;
+        const { focussed, value, error, label } = this.state;
+        const fieldClassName = `field ${(locked ? focussed : focussed || value) && 'focussed'} ${locked && !focussed && 'locked'}`;
+        return (
+            <div className={fieldClassName}>
+                <input 
+                id={id}
+                value={value} 
+                className=''  
+                type={type} 
+                placeholder={focussed ? '' : label}
+                onChange={this.onChange}
+                onFocus={() => !locked && this.setState({ focussed: true })}
+                onBlur={() => !locked && this.setState({ focussed: false })}
+                ></input>
+                <label htmlFor={id} className={error && 'error'}>
+                    {error || label}
+                </label>
+            </div>
+        )
+    }
+}
+
+export default InputField
--- src/modules/settings/index.js
+++ src/modules/settings/index.js
@@ -4,6 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheck, faEye, faX, faComment } from '@fortawesome/free-solid-svg-icons'
 import Breadcrumb from '../generic/breadcrumb';
+import InputField from '../generic/input';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import placeholder from '../../assets/img/placeholder.jpeg'
+
 
 const applicants = [
     {name: "Allison Smith", course: "MS-ME", datesubmitted: "2-23-2023"},
@@ -17,7 +23,11 @@ class Settings extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          showModal: false
+          showModal: false,
+          name: null,
+          errorName: null,
+          email: null,
+          errorEmail: null
         };
       }
     render() {
@@ -30,34 +40,53 @@ class Settings extends Component {
                     header={"Settings"}
                     subheader={"View Account Settings"}/>
 
-                <div className='applicanttitle' >
-                    <p>Name</p>
-                    <p>Course</p>
-                    <p>Date Submitted</p>
-                    <p >Action</p>
-                </div>
-                <div className='applicantData'>
-                {
-                    applicants.map((applicant, idx) => (
-                       
-                            <div key={idx}>
-                                <p>{applicant.name}</p>
-                                <p>{applicant.course}</p>
-                                <p>{applicant.datesubmitted}</p>
-                                <p> 
-                                    <FontAwesomeIcon icon={faCheck} size="sm" style={{color: "#2ead43", margin:"0px 5px", cursor: "pointer"}} />
-                                    <FontAwesomeIcon icon={faEye} onClick={()=>this.setState({showModal:true})} size="sm" style={{color: "#66a5e2", margin:"0px 5px", cursor: "pointer"}}/>
-                                    <FontAwesomeIcon icon={faX} size="sm" style={{color: "#ff0808", margin:"0px 5px", cursor: "pointer"}}/>
-                                    <FontAwesomeIcon icon={faComment} size="sm" style={{color: "#71abe4", margin:"0px 5px", cursor: "pointer"}}/>
-                                </p>
-                            </div> 
-                        
-                    ))
-                }  
+                <div className='containerBlue'>
+                    <Container>
+                        <Row className='break'>
+                            <Col md={3}>
+                            <h4>General Setting</h4>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className='imageCircle'>
+                                <img className='circle' src={placeholder}></img>
+                            </Col>
+                            <Col>
+                                <span className='imageText'>This will be the profile picture displayed</span>
+                            </Col>
+                        </Row>
+                        <Row>
+                            <Col className=''>
+                                <InputField
+                                id={1}
+                                type={'name'}
+                                label={'Name'}
+                                locked={false}
+                                active={false}
+                                onChange={(name, errorName) => {
+                                    this.setState({
+                                      name, errorName
+                                    })
+                                  }}
+                                />
+                            </Col>
+                            <Col>
+                            <InputField
+                                id={1}
+                                type={'email'}
+                                label={'Email'}
+                                locked={false}
+                                active={false}
+                                onChange={(email, errorEmail) => {
+                                    this.setState({
+                                        email, errorEmail
+                                    })
+                                  }}
+                                />
+                            </Col>
+                        </Row>
+                    </Container>
                 </div> 
-                {
-                    console.log("show", showModal)
-                }
                 {/* <Details
                 show={showModal}
                 onHide={()=>this.setState({
